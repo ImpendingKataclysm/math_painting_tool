bbe7fcb53b4a007c34db79083df9cf3d7c166fe7
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-20 00:53:59 -0700

    Draw shapes to the canvas

--- shapes.py
+++ shapes.py
@@ -3,23 +3,19 @@ class Shape:
     An object to be rendered on a canvas. Attributes include x- and y-coordinates
     and a color.
     """
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, width, height, color):
+        self.height = height
+        self.width = width
         self.x = x
         self.y = y
         self.color = color
 
-
-class Square(Shape):
-    """
-    Extends the Shape class to render a Square object with a side length. Includes
-    a method for drawing the Square to a canvas
-    """
-    def __init__(self, side_length, x, y, color):
-        super().__init__(x, y, color)
-        self.side_length = side_length
-
     def draw(self, canvas):
-        pass
+        """
+        Draw the shape on the given canvas
+        """
+        # Change a slice of the canvas RGB array with new values
+        canvas.data[self.x:self.x + self.height, self.y:self.y + self.width] = self.color
 
 
 class Rectangle(Shape):
@@ -27,10 +23,14 @@ class Rectangle(Shape):
     Extends the Shape class to render a Rectangle object with a width and height.
     Includes a method for drawing the Rectangle to a canvas
     """
-    def __init__(self, width, height, x, y, color):
-        super().__init__(x, y, color)
-        self.width = width
-        self.height = height
+    def __init__(self, x, y, width, height, color):
+        super().__init__(x, y, width, height, color)
 
-    def draw(self, canvas):
-        pass
+
+class Square(Rectangle):
+    """
+    Extends the Shape class to render a Square object with a side length. Includes
+    a method for drawing the Square to a canvas
+    """
+    def __init__(self, x, y, side_length, color):
+        super().__init__(x, y, side_length, side_length, color)
