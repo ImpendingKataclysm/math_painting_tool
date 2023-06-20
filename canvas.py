ad76ff6194fe212adfb9900586c505dd544c9659
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-20 00:18:40 -0700

    Defined a class for creating the Canvas and rendering objects on it

---
+++ canvas.py
@@ -0,0 +1,11 @@
+class Canvas:
+    """
+    Canvas object used in the creation of images
+    """
+    def __init__(self, width, height, color):
+        self.color = color
+        self.height = height
+        self.width = width
+
+    def make(self, image_path):
+        pass
